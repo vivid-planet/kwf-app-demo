207c2b4fa2010c1f7b46a6997a8288427d7383c1
Niko Sams <ns@vivid-planet.com>
2011-10-17 14:15:12 +0200

    make ist runnable standalone

--- bootstrap.php
+++ bootstrap.php
@@ -1,19 +1,14 @@
 <?php
 chdir(dirname(__FILE__));
-if (file_exists('include_path')) {
-    $path = str_replace('%vkwf_branch%', trim(file_get_contents('vkwf_branch')), trim(file_get_contents('include_path')));
-} else {
-    $path = dirname(__FILE__).'/vkwf-lib';
-}
 $include_path  = get_include_path();
 $include_path .= PATH_SEPARATOR . 'controllers';
 $include_path .= PATH_SEPARATOR . 'models';
 $include_path .= PATH_SEPARATOR . 'components';
 $include_path .= PATH_SEPARATOR . 'app';
 set_include_path($include_path);
-require_once $path.'/Vkwf/Setup.php';
-Vkwf_Setup::setUp();
-Vkwf_Setup::dispatchMedia();
+require_once 'kwf-lib/Kwf/Setup.php';
+Kwf_Setup::setUp();
+Kwf_Setup::dispatchMedia();
 Kwf_Assets_Loader::load();
 
 $front = Kwf_Controller_Front::getInstance();
