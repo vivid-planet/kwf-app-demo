8d5e5dfcc76530a0ffe6dbc4b708a4903c2eb3ce
Niko Sams <ns@vivid-planet.com>
2014-01-15 14:43:56 +0100

    update to 3.6

--- bootstrap.php
+++ bootstrap.php
@@ -1,15 +1,8 @@
 <?php
 chdir(dirname(__FILE__));
-$include_path  = get_include_path();
-$include_path .= PATH_SEPARATOR . 'controllers';
-$include_path .= PATH_SEPARATOR . 'models';
-$include_path .= PATH_SEPARATOR . 'components';
-$include_path .= PATH_SEPARATOR . 'app';
-set_include_path($include_path);
 require_once 'kwf-lib/Kwf/Setup.php';
 Kwf_Setup::setUp();
 Kwf_Setup::dispatchMedia();
-Kwf_Assets_Loader::load();
 
 $front = Kwf_Controller_Front::getInstance();
 $response = $front->dispatch();
